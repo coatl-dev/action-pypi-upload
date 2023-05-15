b4d48e1286013e24d521fd37e796bda13d62bab1
César Román <thecesrom@gmail.com>
2023-05-15 12:25:21 -0700

    refactor(action): mirror workflows/pypi-upload
    rename inputs:
    - user to username
    remove inputs:
    - outdir
    - verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,27 +6,20 @@ set -Eeuo pipefail
 export PATH="/usr/bin:${PATH}"  # To find `id`
 source /etc/profile  # Makes python and other executables findable
 
-if [[ -n ${INPUT_OUTDIR} ]]; then
-    python -m build --outdir "${INPUT_OUTDIR}"
-fi
+python -m build
 
 if [[ ${INPUT_CHECK,,} != "no" ]] ; then
-    python -m twine check "${INPUT_OUTDIR%%/}"/*
+    python -m twine check dist/*
 fi
 
 if [[ ${INPUT_REGISTER,,} != "no" ]] ; then
-    TWINE_USERNAME="$INPUT_USER" \
+    TWINE_USERNAME="$INPUT_USERNAME" \
     TWINE_PASSWORD="$INPUT_PASSWORD" \
     TWINE_REPOSITORY_URL="$INPUT_URL" \
-        python -m twine register "${INPUT_OUTDIR%%/}"/*.tar.gz
+        python -m twine register dist/*.tar.gz
 fi
 
-EXTRA_ARGS=
-if [[ ${INPUT_VERBOSE,,} != "no" ]] ; then
-    EXTRA_ARGS="--verbose"
-fi
-
-TWINE_USERNAME="$INPUT_USER" \
+TWINE_USERNAME="$INPUT_USERNAME" \
 TWINE_PASSWORD="$INPUT_PASSWORD" \
 TWINE_REPOSITORY_URL="$INPUT_URL" \
-  python -m twine upload "${EXTRA_ARGS}" "${INPUT_OUTDIR}"/*
+  python -m twine upload --verbose dist/*
